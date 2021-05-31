7b6025ba5673e850b1ff60a0a74c37c33f2235a9
Nayeli Pleskac <pleskac510@gmail.com>
2021-05-31 11:32:03 -0700

    blockchain tampering

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,8 @@
 
 #block is the transfer of money or data
 #the hash is the code for that block
+#each block uses the previous block, so tampering 
+#is detected when the "chain" is disrupted or a break in the chain is visible.
 from cryptography.hazmat.primitives import hashes
 digest = hashes.Hash(hashes.SHA256())
 digest.update(b"abc") 
@@ -11,10 +13,11 @@ hash = digest.finalize()
 
 class someClass:
     string = None
+    num = 23987
     def __init__(self, mystring):
         self.string = mystring
     def __repr__(self):
-        return self.string
+        return self.string + '^^^' + str(self.num)
 
 class CBlock:
     data = None
@@ -32,10 +35,9 @@ class CBlock:
         return digest.finalize()
 if __name__ == '__main__':
     root = CBlock('I am root', None)
-    B1 = CBlock('I am a child', root)
-    
+    B1 = CBlock(b'I am a child', root)
     B2 = CBlock('I am a B1\'s brother', root)
-    B3 = CBlock(12345, B1)
+    B3 = CBlock(12354, B1)
     B4 = CBlock(someClass('Hi there!'), B3)
     B5 = CBlock("top block", B4)
 
@@ -48,6 +50,13 @@ if __name__ == '__main__':
     B3.data = 12345
     # print(B4.previousBlock.data)
     if B4.previousBlock.computeHash() == B4.previousHash:
+        print('error, couldn\'t detect tampering')
+    else:
+        print('success, tampering detected')
+    print(B4.data)
+    B4.data.num = 99999
+    print(B4.data)
+    if B5.previousBlock.computeHash() == B5.previousHash:
         print('error, couldn\'t detect tampering')
     else:
         print('success, tampering detected')
